aceb109309ef9bbfaf33d9995f9831d7f4f50af3
Đào Quốc Trượng <101645585+truong11dqtfmb@users.noreply.github.com>
2024-05-22 18:27:50 +0700

    es6

--- es6.js
+++ es6.js
@@ -1,146 +1,144 @@
-const xinSdt = () => {
-    let sdt;
-    console.log('1 - Gọi điện cho thằnng bạn XYZ, cho tao xin số của thằng ABC');
+// 1. Let,const
+// 2. Template literals
+// 3. Multi-line String
+// 4. Arrow function(không có context)
+// 5. Classes
+// 6. Default parameter values
+// 7. Destructuring
+// 8. Rest parameters
+// 9. Spread
+// 10. Enhanced object literals
+// 11. Tagger template literals
+// 12. Modules
 
-    console.log('2 - XYZ đang tìm số của ABC cho bạn');
-    
-    // Giả sử thời gian tìm số  điện thoại của ABC mất khoảng 1s
-    setTimeout(() => {
-        sdt = 123456789;
-        console.log('3 - XYZ đã tìm thấy số của ABC');
-    }, 1000);
+// Babel
 
-    console.log(`4 - Số điện thoại của ABC là ${sdt}`);
+//   1. Let,const
+/*
+ * 1. var / let, const: Scope, Hosting
+ * 2. const / var,let: assignment
+ * 
+ */
+
+// To object:
+const sum = (a,b) => ({a: a, b:b});
+
+console.log(sum(1,4));
+
+
+// One parameter:
+const logger = log => console.log(log);
+logger('Message...')
+
+
+// Các loại function trong JS:
+// 1. Declaration Function:
+function sayHello() {
+  const num = 11;
+  console.log(`Hello 0 ${num}`);
+}
+
+// 2. Expression Function:
+const sayHello1 = function() {
+  console.log('Hello 1 !');
 };
 
-// Gọi hàm để xem kết quả
-xinSdt();
-
-
-
-
-
-
-// Execute the function "doThis" with another function as parameter, in this case "andThenThis". doThis will execute whatever code it has and when it finishes it should have "andThenThis" being executed.
-doThis(andThenThis)
-// Inside of "doThis" it's referenced as "callback" which is just a variable that is holding the reference to this function
-function andThenThis() {
-  console.log('and then this')
-}
-// You can name it whatever you want, "callback" is common approach
-function doThis(callback) {
-  console.log('this first')
-  callback()
-  
-  // the '()' is when you are telling your code to execute the function reference else it will just log the reference callback()
-}
-
-
-
-
-// Arrow function
-const gfg = () => {
-  console.log("Hi from GeekforGeeks!");
+// 3. Arrow Function:
+const sayHello2 = () => {
+  console.log('Hello 2 !');
 };
 
-gfg();
+// 4. Immediately Invoked :function được gọi ngay sau khi được định nghĩa.
+(function() {
+  console.log('Hello 3 !');
+})();
 
 
-// keyword
-function greet() {
-  console.log("Hello, my name is " + this.name);
-}
 
-const person = {
-  name: "John",
-  sayHello: greet,
+
+let name = 'Javascript';
+let price = 1100;
+
+let course0 = {
+  name: name,
+  price: price,
+  getName: function() {
+    return name;
+  }
+};
+console.log(course0);
+
+let course1 = {
+  name,
+  price,
+  getName() {
+    return name;
+  }
+};
+console.log(course1);
+
+let fieldName = 'name';
+let fieldPrice = 'price';
+
+let course2 = {
+  [fieldName]: 'Java',
+  [fieldPrice]: 1000,
 };
 
-const anotherPerson = {
-  name: "Alice",
-};
 
-greet(); // Output: Hello, my name is undefined
-person.sayHello(); // Output: Hello, my name is John
-greet.call(anotherPerson); // Output: Hello, my name is Alice
-
-// // Const 
-// const name = 'Mukul';
-// console.log(name); 
-// // Will print 'Mukul' to the console.
- 
-// // Trying to reassign a const variable
-// // name = 'Rahul';
-// // console.log(name);
+console.log(course2);
 
 
 
-// Object Destructuring
-const college = {
-    name : 'DTU',
-    est : '1941',
-    isPvt : false
-};
-let {name, est, isPvt} = college;
-console.log(name, est, isPvt);
- 
-// Array Destructuring
-const arr = ['lionel', 'messi', 'barcelona'];
-let[value1,value2,value3] = arr;
-console.log(value1, value2, value3);
-
-
-
-// classes in ES6
-class Vehicle{
-    constructor(name,engine){
-        this.name = name;
-        this.engine = engine;
-    }
+// Default value:
+function loggerDefaultValue(log){
+  if(typeof log === 'undefined'){
+    log = 'Gia tri mac dinh';
+  }
+  console.log(log);
 }
- 
-const bike1 = new Vehicle('Ninja ZX-10R','998cc');
-const bike2 = new Vehicle('Duke','390cc');
- 
-console.log(bike1.name); // Ninja ZX-10R
-console.log(bike2.name); // Duke
 
-
-
-// Rest parameter and spread operator in ES6
-function fun(...input){
-    let sum = 0;
-    for(let i of input){
-        sum += i;
-    }
-    return sum;
+function loggerDefaultValueES6(log = 'Gia tri mac dinh'){
+  console.log(log);
 }
-console.log(fun(1,2)); // 3
-console.log(fun(1,2,3)); // 6 
-console.log(fun(1,2,3,4,5)); // 15  
+loggerDefaultValueES6(undefined);
+loggerDefaultValueES6();
+loggerDefaultValueES6('Message');
 
-
-
-
-
-// Hoisting
-// function codeHoist() {
-//     a = 10;
-//     let b = 50;
-// }
-// codeHoist();
-
-// console.log(a); // 10
-// console.log(b); 
-
-
-
-const getData = async () => {
-    let data = "Hello World";
-    return data + ' dqt';
+function loggerDefaultOption(log, type = 'log'){
+  console[type](log);
 }
- 
-getData().then(data => console.log(data));
+loggerDefaultOption('Message', 'error');
+
+
+
+// destructuring & rest parameters:
+
+// Array:
+let array = ['a', 'b','c'];
+
+let [a,b,c] = array;
+// let [a,,c] = array;
+// let [a,...rest] = array;
+// console.log(rest);
+console.log(a,b,c);
+
+// Object: 
+let employee = {
+  fullName: 'John',
+  age: 21,
+  image: 'picture.png'
+} 
+
+// let {fullName, age, image} = employee;
+// console.log(fullName, age, image);
+
+let {fullName, ...newEmployee} = employee;
+console.log(newEmployee)
+
+
+
+
 
 
 
